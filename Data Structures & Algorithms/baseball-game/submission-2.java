ecb4a596678952408f5f219e6013edeaa94090c1
Yana Singh <yanasingh888@gmail.com>
2026-09-19 23:37:52 +0530

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(String op : operations){
+            if(op.equals("C")) stack.pop();
+            else if(op.equals("D")) stack.push(stack.peek()*2);
+            else if(op.equals("+")){
+                int first = stack.pop();
+                int second = stack.peek();
+
+                stack.push(first);
+                stack.push(first + second);
+            }else{
+                stack.push(Integer.parseInt(op));
+            }
+        }
+        int sum = 0;
+
+        while(!stack.isEmpty()){
+            sum+=stack.pop();
+        }
+        return sum;
+    }
+}
+    
