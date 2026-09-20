76cff2e938f218811365d72b8ff9354b4cbc072f
Yana Singh <yanasingh888@gmail.com>
2026-09-20 15:20:12 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int i = 0, j = 0;
+        int[] ans = new int[2 * n];
+
+        while(i < n) {
+            ans[j] = nums[i];
+            i++;
+            j++;
+            if (i == n && j != 2 * n){
+                i = 0;
+            }
+        }
+
+        return ans;
+    }
+}
